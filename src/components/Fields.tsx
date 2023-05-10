c13479c6e877a3ec3e78c48515667d5d3053d0be
rodrigojmayer <rodrigojmayer@gmail.com>
2023-05-10 22:55:08 +0200

    progress fields finish buttons

--- src/components/Fields.tsx
+++ src/components/Fields.tsx
@@ -1,46 +1,29 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { makeStyles } from 'tss-react/mui';
-// import MenuList from '@mui/material/MenuList/MenuList';
 import Box from '@mui/material/Box';
-// import Button from '@mui/material/Button';
 import { Container, Typography, Grid } from '@mui/material';
 import Modal from '@mui/material/Modal';
 import Paper from '@mui/material/Paper/Paper';
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd"
 
-// import { PlusButton, MinusButton } from './Buttons';
-import { Button, IconButton  } from '@mui/material';
+import { OkButton, CancelButton } from './Buttons';
+import {  IconButton  } from '@mui/material';
 import RemoveCircleTwoToneIcon from '@mui/icons-material/RemoveCircleTwoTone';
 import ControlPointTwoToneIcon from '@mui/icons-material/ControlPointTwoTone';
 import List from '@mui/material/List/List';
-import { ListItem } from 'material-ui';
-// import ButtonGroup from '@mui/material/ButtonGroup';
 
 
 const useStyles = makeStyles()({
-    // droppable: {
-    //     display: "flex",
-    //     // width: "100%",
-    // },
     table: {
-        // position: "absolute",
-        // bottom: 48,
-        // zIndex: 500,
         width: "calc(100% - 6px)",
-        // height: "250px",
-        // minHeight: "10%",
         margin: "3px",
         padding: "6px 0",
-        // backgroundColor: "red !important"
         borderRadius: "10px",
         backgroundColor: "rgb(69, 144, 186)",
         display: "flex",
         flexDirection: "column",
-        // justifyContent: "space-evenly",
         alignItems: "center",
-        // gap: "9px",
-        
     },
     buttonsGroup: {
         width: "100%",
@@ -49,28 +32,14 @@ const useStyles = makeStyles()({
     buttonFields: {
         backgroundColor: "white",
         width: "calc(100% - 12px)",
-        // width: "auto",
         margin: "9px",
-        // padding: "0 5px",
         paddingLeft: "8px",
         height: "32px",
         borderRadius: "10px",
         display: "flex",
         justifyContent: "space-between",
         alignItems: "center",
-        // fontFamily: [
-        //   '"Asap Condensed"',
-        // ].join(','),
-        // fontSize: "20px",
         
-    },
-
-
-    widgets: {
-
-    },
-    widget: {
-
     },
     page: {
         display: "inline-block",
@@ -78,7 +47,6 @@ const useStyles = makeStyles()({
         width: "30%",
         height: "30%",
         backgroundColor: "white",
-       
     },
     dropped_widget: {
         color: "red",
@@ -90,7 +58,6 @@ const useStyles = makeStyles()({
         '& svg': {
             width: "32px", 
             height: "32px",
-
         }
     },
     plusIcon: {
@@ -102,33 +69,25 @@ const useStyles = makeStyles()({
             height: "32px",
         }
     },
+    finishButtons: {
+        display: "flex",
+        justifyContent:  "center",
+        gap: 30,
+        margin: "20px",
+    }
 })
 
 const style = {
     position: 'absolute',
     display: "flex",
     justifyContent: "center",
-    // left: "50%",
-    // transform: "translate(-50%, 0%)",
     top: 74,
     width: "100%",
-    // height: "90%",
-    // backgroundColor: "rgb(45,72, 91, 1)",
-    // margin: "auto",
-    // padding: "3px",
-    
-    // color: "white",
     overflowX: "hidden",
 };
 const style2 = {
-    // position: 'absolute',
-    // display: "flex",
-    // justifyContent: "center",
-    // left: "50%",
-    // transform: "translate(-50%, 0%)",
     top: 74,
     width: "calc(100% - 32px)",
-    // height: "70%",
     height: "520px",
     backgroundColor: "rgb(45,72, 91, 1)",
     margin: "auto",
@@ -139,14 +98,6 @@ const style2 = {
 
 };
 
-// type Data = {
-//     id: number;
-//     product: string;
-//     amount: number;
-//     unit: string;
-//     category: string;
-//     sub_category: string;
-//   }
 
 interface ColumnData {
     id: number;
@@ -161,28 +112,15 @@ interface ChildProps {
     columns: ColumnData[]
 }
 
-
 export default function Fields({ open, handleClose, columns }: ChildProps) {
-
 
     const { classes } = useStyles()
     const close = () => {
         handleClose(false)
     }
 
-
-
-    // useEffect(() => {
-    //     console.log("Fields columns: ", columns)
-        
-    // }, [])
-
     const [orderedFields, setOrderedFields] = useState(columns)
-    const [unsetFields, setUnsetFields] = useState<ColumnData[]>([])
-     
-    // const handleDragStart = () => {
-
-// }                               
+    const [unsetFields, setUnsetFields] = useState<ColumnData[]>([])                               
     const removeField = (e: React.MouseEvent<HTMLButtonElement>)  => {
         let orderedArray = Array.from(orderedFields)
         const unsetArray = Array.from(unsetFields)
@@ -212,104 +150,43 @@ export default function Fields({ open, handleClose, columns }: ChildProps) {
         setUnsetFields(unsetArray)
     }
     const handleDragEnd = (result: any) => {
-        console.log("result: ", result)
         if (!result.destination) return;
         const items = Array.from(orderedFields);
-        console.log("items: ", items)
-        // console.log("items: ", items)
         const [reorderData] = items.splice(result.source.index,1);
         items.splice(result.destination.index, 0, reorderData);
         setOrderedFields(items)
     }
-
-
-
     return (
         <Modal
-            open={open} 
-            onClose={close}
-        > 
+        open={open} 
+        onClose={close}> 
             <Box sx={style}>
                 <Box sx={style2}>
-                
-                    {/* <Typography align="center" variant="h5">
+                    <Typography align="center" variant="h5">
                         Fields
                     </Typography>
-
                     <Grid container>
-                <Grid item xs={6} >
-                    <Typography align="center" >
-                        Table order
-                    </Typography>
-                </Grid>
-                <Grid item xs={6} >
-                </Grid>
-                <Grid item xs={6} >
-                    <Box className={classes.table}>
-                        {columns.map((column) => (
-                            <Button
-                            key={column.dataKey} 
-                            className={classes.buttonFields}
-                            endIcon={<MinusButton />}
-                            >
-                                {column.label}
-                            </Button>
-                        ))}
-
-                    </Box>
-                </Grid>
-                <Grid item xs={6} >
-                    <Box className={classes.table}>
-                    {columns.map((column) => (
-                            <Button
-                            key={column.dataKey} 
-                            className={classes.buttonFields}
-                            endIcon={<PlusButton />}
-                            >
-                                {column.label}
-                            </Button>
-                        ))}
-                    </Box>
-                </Grid>
-                </Grid> */}
-
-
-                <Typography align="center" variant="h5">
-                    Fields
-                </Typography>
-                                <Grid container>
-                                    <Grid item xs={6} >
-                                        <Typography align="center" >
-                                            Table order
-                                        </Typography>
-                                    </Grid>
-                                    <Grid item xs={6} >
-                                        <Typography align="center" >
-                                            Hidden fields
-                                        </Typography>
-                                    </Grid>
-                <Grid item xs={6} >
-                <DragDropContext 
-                // onDragStart={handleDragStart}
-                onDragEnd={handleDragEnd}
-                >
-                {/* <Box  className={classes.droppable}> */}
-                
-                    <Droppable
-                   
-                    droppableId="list"
-                    >
-                       
-
-
-                        {(provided) => (
-                            <List
-                            className={classes.table}
-                                {...provided.droppableProps}
-                                ref={provided.innerRef}
-                                // sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
-                            >
-
+                        <Grid item xs={6} >
+                            <Typography align="center">
+                                Table order
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={6} >
+                            <Typography align="center">
+                                Hidden fields
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={6} >
+                            <DragDropContext 
+                            // onDragStart={handleDragStart}
+                            onDragEnd={handleDragEnd}>
+                                <Droppable
+                                droppableId="list">
+                                    {(provided) => (
+                                        <List
+                                        className={classes.table}
+                                        {...provided.droppableProps}
+                                        ref={provided.innerRef}>
                                             {orderedFields.map((column, index) => (
                                                 <Draggable 
                                                 key={column.id} 
@@ -321,97 +198,52 @@ export default function Fields({ open, handleClose, columns }: ChildProps) {
                                                         {...provided.draggableProps}
                                                         {...provided.dragHandleProps}
                                                         elevation={2}
-                                                        // key={column.dataKey} 
-                                                        className={classes.buttonFields}
-                                                        >
-                                                                
+                                                        className={classes.buttonFields}>
                                                             <Typography noWrap>
                                                                 {column.label}
                                                             </Typography>
-                                                            {/* <Button
-                                                            > */}
                                                             <IconButton
                                                             onClick={removeField}
                                                             className={classes.backPlus}
                                                             id="minusButton"
                                                             value={column.id}>
-                                                                <RemoveCircleTwoToneIcon 
-                                                                />
+                                                                <RemoveCircleTwoToneIcon/>
                                                             </IconButton>
-                                                            {/* </Button> */}
-                                                            
-
-                                                            {/* <MinusButton 
-                                                            onClick={() => removeField()}
-                                                            sizeData={"30px"}
-                                                            /> */}
-                                                        
                                                         </Paper>
                                                     )}
                                                 </Draggable>
                                             ))}
-
-                                {provided.placeholder} 
-                            </List>
-                            
-
-                        )}
-                            
-
-                    </Droppable>
-                    
-                </DragDropContext>
-                    </Grid>
-                                    <Grid item xs={6} >
-                    {/* <Droppable droppableId="list"> */}
-                        {/* {(provided) => ( */}
+                                            {provided.placeholder} 
+                                        </List>
+                                    )}
+                                </Droppable>
+                            </DragDropContext>
+                        </Grid>
+                        <Grid item xs={6} >
                             <List
-                            className={classes.table}
-                                // {...provided.droppableProps}
-                                // ref={provided.innerRef}
-                                // sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
-                            >
-
-                                            {unsetFields.map((column, index) => (
-                                                    // {(provided) => (
-                                                        <Paper
-                                                        // ref={provided.innerRef}
-                                                        // {...provided.draggableProps}
-                                                        // {...provided.dragHandleProps}
-                                                        // elevation={2}
-                                                        // key={column.dataKey} 
-                                                        className={classes.buttonFields}
-                                                        // endIcon={<MinusButton />}
-                                                        >
-                                                        
-                                                        {/* <ListItem> */}
-                                                                
-                                                            {column.label}
-                                                            
-                                                            <IconButton
-                                                            className={classes.plusIcon}
-                                                            onClick={addField}
-                                                            id="plusButton"
-                                                            value={column.id}>
-                                                                <ControlPointTwoToneIcon/>
-                                                            </IconButton>
-                                                        {/* </ListItem> */}
-                                                        
-                                                        </Paper>
-                                                    // )}
-                                                // </Draggable>
-                                            ))}
-
-                                {/* {provided.placeholder}  */}
+                            className={classes.table}>
+                                {unsetFields.map((column, index) => (
+                                    <Paper
+                                    className={classes.buttonFields}>
+                                        <Typography noWrap>
+                                            {column.label}
+                                        </Typography>  
+                                        <IconButton
+                                        className={classes.plusIcon}
+                                        onClick={addField}
+                                        id="plusButton"
+                                        value={column.id}>
+                                            <ControlPointTwoToneIcon/>
+                                        </IconButton>
+                                    </Paper>
+                                ))}
                             </List>
-
-                        {/* )} */}
-
-                    {/* </Droppable> */}
-                                </Grid>
-                    {/* </Box> */}
-                </Grid>
-                teste
+                        </Grid>
+                    </Grid>
+                    <Box className={classes.finishButtons}>
+                        <CancelButton/>
+                        <OkButton/>
+                    </Box>
                 </Box>
             </Box>
         </Modal>
