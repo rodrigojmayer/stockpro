d48976d80282fa0ec0f4024d3e42f96ab681824d
rodrigojmayer <rodrigojmayer@gmail.com>
2023-08-24 21:36:12 +0200

    progress with style slider button

--- src/components/ConfirmDeleteModal.tsx
+++ src/components/ConfirmDeleteModal.tsx
@@ -8,12 +8,56 @@ import {
         } from './Buttons';
 import { useStylesGlobal, modalStyleSaveExternal, modalStyleErrorModalInternal } from '../Styles'
 import Slider from '@mui/material/Slider';
-// import { useState } from 'react';
+import { styled } from '@mui/material/styles';
 
-// import Box from '@mui/material/Box';
-// import Slider from '@mui/material/Slider';
 
-// import Slider from 'react-native-slide-to-unlock';
+const PrettoSlider = styled(Slider)({
+    color: '#52af77',
+    height: 25,
+    borderRadius: '7px',
+    //borderRadius: '0 7px 7px 0',
+    paddingLeft: '40px' ,
+   // paddingRight: '25px',
+    '& .MuiSlider-track': {
+      border: 'none',
+      borderRadius: '7px 0 0 7px',
+    },
+    '& .MuiSlider-thumb': {
+      height: 25,
+      width: 50,
+      marginLeft: '21px',
+      //paddingRight: '25px', 
+      borderRadius: '7px',
+      backgroundColor: '#fff',
+      border: '1px solid currentColor',
+      '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
+        //boxShadow:  'inherit',
+      },
+      '&:before': {
+        display: 'none',
+      },
+    },
+    '& .MuiSlider-valueLabel': {
+      display: 'none',
+      lineHeight: 10.2,
+      fontSize: 12,
+      background: 'unset',
+      padding: 0,
+      width: 32,
+      height: 32,
+      borderRadius: '50% 50% 50% 0',
+      backgroundColor: '#52af77',
+      transformOrigin: 'bottom left',
+      transform: 'translate(50%, -100%) rotate(-45deg) scale(0)',
+      '&:before': { display: 'none' },
+      '&.MuiSlider-valueLabelOpen': {
+        transform: 'translate(50%, -100%) rotate(-45deg) scale(1)',
+      },
+      '& > *': {
+        transform: 'rotate(45deg)',
+      },
+    },
+  });
 
 type ConfirmDeleteModalProps = {
     openConfirmDeleteModal: boolean;
@@ -58,7 +102,7 @@ export default function ConfirmDeleteModal( props: ConfirmDeleteModalProps) {
     const handleSliderChange = (event: Event, newValue: number | number[], activeThumb: number) => {
         const value = typeof newValue === 'number' ? newValue : newValue[activeThumb];
          if (isThumbPressed) {
-            if(valueSlider-5 <= value &&  value <= valueSlider+5){
+            if(valueSlider-20 <= value && value <= valueSlider+20){
                 console.log('Thumb pressed:', value);
                 setValueSlider(value)
             }
@@ -85,7 +129,7 @@ export default function ConfirmDeleteModal( props: ConfirmDeleteModalProps) {
 
                     
                     <Box sx={{ width: 100 }}>
-                        <Slider
+                        <PrettoSlider
                             aria-label="Temperature"
                             // defaultValue={30}
                             value={valueSlider}
@@ -94,6 +138,8 @@ export default function ConfirmDeleteModal( props: ConfirmDeleteModalProps) {
                             // disabled={isThumbPressed}
                             onMouseDown={handleThumbMouseDown} // Attach the event handler when thumb is pressed
                             onMouseUp={handleThumbMouseUp}     // Attach the event handler when thumb is released
+                            onTouchStart={handleThumbMouseDown} // Attach the event handler when thumb is pressed
+                            onTouchEnd={handleThumbMouseUp}     // Attach the event handler when thumb is released
                             onChange={handleSliderChange}
                         />
                     </Box>
