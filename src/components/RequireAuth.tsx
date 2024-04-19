bc5af1df72aec0182911eb90c954ab4d3b5c64b1
rodrigojmayer <rodrigojmayer@gmail.com>
2024-04-19 21:23:20 +0200

    progress with waiting loading page

--- src/components/RequireAuth.tsx
+++ src/components/RequireAuth.tsx
@@ -1,23 +1,43 @@
 import { useLocation, Navigate, useNavigate, Outlet } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { IsLoadingContext } from "../context/IsLoadingContext";
+import { Backdrop, CircularProgress } from "@mui/material";
+import { Suspense } from 'react';
 
 const RequireAuth = () => {
     const { auth, persist } = useAuth();
     const location = useLocation();
-    const [isLoading, setIsLoading] = useState(true);
+    const { isLoading, setIsLoading , } = useContext<any>(IsLoadingContext);
+    const [loading, setLoading] = useState(true);
     const [secondLoad, setSecondLoad] = useState(false);
+    const [render, setRender] = useState("loading");
+    // const [countAuthRenders, setcountAuthRenders] = useState(0);
     const navigate = useNavigate();
 
+    
+    // console.log(" isLoading: ", isLoading)
+    // console.log(" auth: ", auth)
+    console.log(" _id: ", auth._id)
+    // console.log(" auth userNameEmail: ", auth?.userNameEmail)
+
     useEffect(() => {
-        // console.log("RequireAuth auth: ", auth)
-        // console.log("RequireAuth _id: ", auth._id)
-        // console.log("RequireAuth auth userNameEmail: ", auth?.userNameEmail)
+        // setcountAuthRenders(+1)
+        // console.log("useEffect isLoading: ", isLoading)
+        // console.log("useEffect auth: ", auth)
+        console.log("useEffect _id: ", auth._id)
+        // console.log("useEffect auth userNameEmail: ", auth?.userNameEmail)
         if(auth || secondLoad){
-            if (auth._id ){
+            if (auth._id){
+
                 navigate('/')
+                setRender("home")
+            } else {
+                // navigate('/login')
+                setRender("login")
             }
-            setIsLoading(false)
+            // setIsLoading(false)
+            
         } else {
             setSecondLoad(true)
         }
@@ -25,16 +45,30 @@ const RequireAuth = () => {
     // useEffect(() => {
     //     console.log("RequireAuth persist: ", persist)
     // }, [persist])
+    // useEffect(() => {
+    //     console.log("countAuthRenders: ", countAuthRenders)
+    // }, [countAuthRenders])
 
     return (
         <>
-        {
-            isLoading
-                    ? <p></p>
-                    : auth?._id 
-                        ? <Outlet />
-                        : <Navigate to="/login" state={{ from: location }} replace />
-        }
+         {
+            // auth?._id && (isLoading.user || 
+            // isLoading.products ||
+            // isLoading.columns||
+            // isLoading.customColumns)
+                 render==="login"   ? 
+                 <Navigate to="/login" state={{ from: location }} replace />
+                    : (render === "home" && !isLoading.user && !isLoading.products && !isLoading.columns && !isLoading.customColumns )?
+                    // : auth?._id 
+                         <Outlet />
+                        : 
+                    <Backdrop
+                    sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                    open={true} // Loading...
+                  >
+                    <CircularProgress color="inherit" />
+                  </Backdrop>
+        } 
         </>
     )
 }
