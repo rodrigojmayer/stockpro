f8489da1056a7c8da1ecaa699eaa88f18df4122b
rodrigojmayer <rodrigojmayer@gmail.com>
2024-05-03 21:05:01 +0200

    nothing

--- src/main.tsx
+++ src/main.tsx
@@ -19,7 +19,7 @@ import { ColumnsProvider } from './context/ColumnsContext';
 import { ProductsProvider } from './context/ProductsContext';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { CheckListStockProvider } from './context/CheckListStockContext';
- 
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // <React.StrictMode>
     <BrowserRouter>
