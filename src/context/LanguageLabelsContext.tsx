bf4e93b0c0c273657549a7afdea7a20afbccca03
rodrigojmayer <rodrigojmayer@gmail.com>
2024-05-31 18:06:13 +0200

    language labels context update

--- src/context/LanguageLabelsContext.tsx
+++ src/context/LanguageLabelsContext.tsx
@@ -16,6 +16,10 @@ export const LanguageLabelsProvider: React.FC<LanguageLabelsProviderProps> = ({ 
   const [labelsTableProducts, setLabelsTableProducts] = useState({}); // New state for loading status
   const [labelsProfile, setLabelsProfile] = useState({}); // New state for loading status
   const [labelsChangePass, setLabelsChangePass] = useState({}); // New state for loading status
+  const [labelsPreferences, setLabelsPreferences] = useState({}); // New state for loading status
+  const [labelsUsers, setLabelsUsers] = useState({}); // New state for loading status
+  const [labelsManageStock, setLabelsManageStock] = useState({}); // New state for loading status
+  const [labelsManageUser, setLabelsManageUser] = useState({}); // New state for loading status
   
 
   useEffect(() => {
@@ -49,6 +53,44 @@ export const LanguageLabelsProvider: React.FC<LanguageLabelsProviderProps> = ({ 
         new_password: "New password*",
         confirm_new_password: "Confirm password*",
       });
+      setLabelsPreferences({
+        preferences: "Preferences", 
+        language: "Language", 
+        color_mode: "Color mode",  
+      });
+      setLabelsUsers({
+        users: "Users", 
+      });
+      setLabelsManageStock({
+        create_stock: "Create ", 
+        edit_stock: "Edit ", 
+        main_data: "Main data", 
+        name: "Name*", 
+        code: "Code", 
+        amount: "Amount", 
+        measure: "Measure", 
+        category: "Category", 
+        sub_category: "Sub-Categ.", 
+        secondary_data: "Secondary data", 
+        price: "Price", 
+        description: "Description",  
+        alerts: "Alerts", 
+        by_amount: "By amount", 
+        by_date: "By date", 
+        custom_fields: "Custom fields", 
+      });
+      setLabelsManageUser({
+        create_user: "Create user", 
+        edit_user: "Edit user", 
+        access_level: "Access level*", 
+        name: "Name", 
+        last_name: "Last name", 
+        user: "User*", 
+        email: "Email*", 
+        alerts_by_email: "Alerts by email",
+        user_enabled: "User enabled",
+        user_disabled: "User disabled",
+      });
     } 
     
     else if (user.language === 1) {
@@ -63,8 +105,8 @@ export const LanguageLabelsProvider: React.FC<LanguageLabelsProviderProps> = ({ 
       });
       setLabelsTableProducts({
         alerts_on_top: "Alertas primero",
-        manage_columns: "Administrar columnas",
-        custom_fields: "Campos personalizados",
+        manage_columns: "Gestionar campos",
+        custom_fields: "Campos propios",
       });
       setLabelsProfile({
         profile: "Perfil",
@@ -81,10 +123,48 @@ export const LanguageLabelsProvider: React.FC<LanguageLabelsProviderProps> = ({ 
         new_password: "Nueva contraseña*",
         confirm_new_password: "Confirmar contraseña*",
       });
+      setLabelsPreferences({
+        preferences: "Preferencias", 
+        language: "Lenguaje", 
+        color_mode: "Modo",  
+      });
+      setLabelsUsers({
+        users: "Usuarios", 
+      });
+      setLabelsManageStock({
+        create_stock: "Crear ", 
+        edit_stock: "Editar ", 
+        main_data: "Datos básicos", 
+        name: "Nombre*", 
+        code: "Código", 
+        amount: "Cantidad", 
+        measure: "Medida", 
+        category: "Categoría", 
+        sub_category: "Sub-Categ.", 
+        secondary_data: "Datos extra", 
+        price: "Precio", 
+        description: "Descripción",  
+        alerts: "Alertas", 
+        by_amount: "Por cantidad", 
+        by_date: "Por fecha", 
+        custom_fields: "Campos propios", 
+      });
+      setLabelsManageUser({
+        create_user: "Crear usuario", 
+        edit_user: "Editar usuario",
+        access_level: "Nivel de acceso*", 
+        name: "Nombre", 
+        last_name: "Apellido", 
+        user: "Usuario*", 
+        email: "Email*", 
+        alerts_by_email: "Alertas por email",
+        user_enabled: "Usuario habilitado",
+        user_disabled: "Usuario deshabilitado",
+      });
     }
-  }, [user.language]);
+  }, [user.language]); 
 
-  return  (
+  return  ( 
     <LanguageLabelsContext.Provider 
       value={{ 
         labelsMainSearch, 
@@ -92,8 +172,12 @@ export const LanguageLabelsProvider: React.FC<LanguageLabelsProviderProps> = ({ 
         labelsTableProducts, 
         labelsProfile, 
         labelsChangePass, 
-      }}
-    >
-      {children}
-    </LanguageLabelsContext.Provider>)
+        labelsPreferences, 
+        labelsUsers, 
+        labelsManageStock, 
+        labelsManageUser, 
+      }} 
+    > 
+      {children} 
+    </LanguageLabelsContext.Provider>) 
 };
