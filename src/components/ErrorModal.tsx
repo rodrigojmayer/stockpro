be2315aae8508c5caf6eb8d2d56f9a443e31907a
rodrigojmayer <rodrigojmayer@gmail.com>
2024-07-25 18:23:13 +0200

    Italian error modal language

--- src/components/ErrorModal.tsx
+++ src/components/ErrorModal.tsx
@@ -16,6 +16,7 @@ type ErrorModalProps = {
     closeErrorModal: (newData?: boolean) => void;
     errorData: string
 }
+// languagechangessearch
 export default function ErrorModal( props: ErrorModalProps) {
     const { openErrorModal, closeErrorModal, errorData } = props;
     const { classes } = useStylesGlobal();
@@ -293,6 +294,96 @@ export default function ErrorModal( props: ErrorModalProps) {
             // subTitle='You can sign up again'
         } else if (errorData === 'invalid_code'){
             title='Ugyldig kodevalidering'
+            // subTitle='You can sign up again'
+        }
+    } else if (user.language === 3 ) {
+        if(errorData === 'missing_data'){
+            title='Dati richiesti mancanti'
+            subTitle='Nome*'
+        } else if (errorData === 'negative_amount'){
+            title='Carenza'
+            subTitle="L'importo non può essere negativo"
+        } else if (errorData === 'missing_data_user'){
+            title='Dati richiesti mancanti'
+            subTitle='Alias'
+        } else if (errorData === 'missing_user_name'){
+            title='Dati richiesti mancanti'
+            subTitle='Nome*'
+        } else if (errorData === 'missing_user_access_level'){
+            title='Dati richiesti mancanti'
+            subTitle='Livello di accesso*'
+        } else if (errorData === 'missing_user_user'){
+            title='Dati richiesti mancanti'
+            subTitle='Utente*'
+        } else if (errorData === 'missing_user_name_email'){
+            title='Dati richiesti mancanti'
+            subTitle='Nome utente o email*'
+        } else if (errorData === 'missing_user_password'){
+            title='Dati richiesti mancanti'
+            subTitle="Parola d'ordine*"
+        } else if (errorData === 'missing_email'){
+            title='Dati richiesti mancanti'
+            subTitle='E-mail*'
+        } else if (errorData === 'confirm_password_must_match'){
+            title='La password di conferma deve corrispondere'
+            subTitle='Conferma password*'
+        } else if (errorData === 'missing_terms_and_privacy'){
+            title='È necessario accettare i termini e la privacy'
+            subTitle='Termini e privacy*'
+        } else if (errorData === 'invalid_email_format'){
+            title='Formato email non valido'
+            subTitle=''
+        } else if (errorData === 'email_duplicated'){
+            title=''
+            subTitle='Indirizzo email già in uso'
+        } else if (errorData === 'user_duplicated'){
+            title=''
+            subTitle='Utente già in uso'
+        } else if (errorData === 'user_deleted'){
+            title=''
+            subTitle='Utente eliminato'
+        }  else if (errorData === 'user_disabled'){
+            title=''
+            subTitle='Utente disabilitato'
+        } else if (errorData === 'login_failed'){
+            title='Accesso non riuscito'
+            subTitle='Utente o password errati'
+        } else if (errorData === 'missing_actual_pass'){
+            title='Dati richiesti mancanti'
+            subTitle='Password effettiva*'
+        } else if (errorData === 'missing_new_pass'){
+            title='Dati richiesti mancanti'
+            subTitle='Nuova password*'
+        } else if (errorData === 'missing_confirm_new_pass'){
+            title='Dati richiesti mancanti'
+            subTitle='Conferma la nuova password*'
+        } else if (errorData === 'not_confirmed_pass'){
+            title="Non c'è alcuna coincidenza"
+            // title='Not confirmed password'
+            subTitle='La conferma della password non corrisponde'
+        }  else if (errorData === 'invalid_password'){
+            title=''
+            subTitle='Password effettiva errata'
+        }  else if (errorData === 'email_not_found'){
+            title=''
+            subTitle='E-mail non trovata'
+        } else if (errorData === 'expired_validation'){
+            title='Collegamento di attivazione non valido'
+            subTitle='Puoi iscriverti di nuovo'
+        } else if (errorData === 'invalid_user_format'){
+            title='Formato utente non valido'
+            subTitle='Contiene caratteri non validi'
+        } else if (errorData === 'invalid_pass_format'){
+            title='Formato della password invalido'
+            subTitle='Deve contenere almeno 6 caratteri'
+        } else if (errorData === 'missing_verification_code'){
+            title='Dati richiesti mancanti'
+            subTitle='Codice di verifica*'
+        } else if (errorData === 'expired_code_validation'){
+            title='Convalida del codice scaduta'
+            // subTitle='You can sign up again'
+        } else if (errorData === 'invalid_code'){
+            title='Convalida del codice non valida'
             // subTitle='You can sign up again'
         }
     }
