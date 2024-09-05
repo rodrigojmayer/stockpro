25f4beea6b5af2ba47d9f40bb44ea490e345b2dc
rodrigojmayer <rodrigojmayer@gmail.com>
2024-09-05 18:33:48 +0200

    fixed update category load without reload the page

--- src/components/administrator/ManageCategory.tsx
+++ src/components/administrator/ManageCategory.tsx
@@ -163,6 +163,7 @@ export default function ManageCategory(
                         ...prevLoading,
                         categories: loadingSuccess,
                     }));
+                    setCategoryLoad(categoryTemp)
                     
                 }
             } 
